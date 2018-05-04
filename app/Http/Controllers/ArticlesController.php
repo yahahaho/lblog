ee593ccf6624de89dc83712fd4aef1659ef7ecac
yahahaho <tchuan666@icloud.com>
2018-05-04 17:02:44 +0800

    update article

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -68,7 +68,8 @@ class ArticlesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $article = Article::findOrFail($id);
+        return view('articles.edit',compact('article'));
     }
 
     /**
@@ -80,7 +81,15 @@ class ArticlesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+                'title' => 'required|max:50',
+            ]);
+        $article = Article::findOrFail($id);
+        $article->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        return redirect()->route('articles.index');
     }
 
     /**
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,6 +8,7 @@
 		<div style="border:1px solid gray;margin-top:20px;padding:20px;">
 			<h2>{{ $article->title }}</h2>
 			<p>{{ $article->content }}</p>
+			<a href="{{ route('articles.edit',$article->id) }}">Edit</a>
 		</div>
 	@endforeach
 @endsection
